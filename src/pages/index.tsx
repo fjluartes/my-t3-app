98596cd9fc37a32d80c41341c564966f292ac2b6
fred <fjluartes@gmail.com>
2023-06-10 13:15:01 +0800

    Removes style in SignInButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,8 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          {!user.isSignedIn && <SignInButton style={{ color: "white" }} />}
-          {!!user.isSignedIn && <SignOutButton style={{ color: "white" }} />}
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
         </div>
       </main>
     </>
